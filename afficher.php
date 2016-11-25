000be30e039d1fa5b6aa021a30b86ba9c5ce443f
Amira111 <amirajlassi190@yahoo.fr>
2016-11-25 01:26:10 +0100

    Update afficher.php

--- afficher.php
+++ afficher.php
@@ -1,3 +1,14 @@
 <?php
-echo "hello world";
+$servername = "localhost";
+$username = "amira";
+$password = "amira";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 ?>
