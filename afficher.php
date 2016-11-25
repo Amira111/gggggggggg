d97f1952138e53eeafd975e7b696de237d383884
Amira111 <amirajlassi190@yahoo.fr>
2016-11-25 01:35:06 +0100

    Update afficher.php

--- afficher.php
+++ afficher.php
@@ -2,13 +2,27 @@
 $servername = "localhost";
 $username = "amira";
 $password = "amira";
-
+$dbname = "logiciels";
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password,$dbname);
 
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
+
+
+$sql = "CREATE TABLE os (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(30) NOT NULL,
+nbuser VARCHAR(30) NOT NULL,
+nbversion VARCHAR(50) NOT NULL,
+nbsmart VARCHAR(30) NOT NULL 
+)";
+if (mysqli_query($conn, $sql)) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
 ?>
