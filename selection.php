5a79019aba470a7469ac8e39cddf4fcd8c383ecd
Amira111 <amirajlassi190@yahoo.fr>
2016-11-25 01:55:40 +0100

    Create selection.php

---
+++ selection.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "amira";
+$password = "amira";
+$dbname = "logiciels";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+$sql = "SELECT * FROM os where name like Android";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+$row = mysqli_fetch_assoc($result);
+
+echo "".$row["id"]." ".$row["name"]." ".$row["nbuser"]."";
+}
+$conn->close();
+?>
