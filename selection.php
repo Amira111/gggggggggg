338d04cfa47df1652d76746397993f097f6390a8
Amira111 <amirajlassi190@yahoo.fr>
2016-11-26 22:14:53 +0100

    Update selection.php

--- selection.php
+++ selection.php
@@ -6,7 +6,7 @@ $dbname = "logiciels";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-$sql = "SELECT * FROM os where name like Android";
+$sql = "SELECT * FROM os where name like 'Android'";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
 $row = mysqli_fetch_assoc($result);
