c0831d4546981e033bc35d1e6ae292ef254196e5
Amira111 <amirajlassi190@yahoo.fr>
2016-11-26 23:34:53 +0100

    Update selection.php

--- selection.php
+++ selection.php
@@ -1,11 +1,15 @@
 <?php
+define('SERVER_NAME', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('USERNAME', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('PASSWORD', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_NAME', getenv('OPENSHIFT_MYSQL_DB_HOST'));
 $servername = "172.30.170.107";
 $username = "amira";
 $password = "amira";
 $dbname = "logiciels";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect(SERVER_NAME,USERNAME , PASSWORD, DB_NAME);
 $sql = "SELECT * FROM os where name like 'Android'";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
