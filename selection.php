aff378a4d7cd2033159d7531c3fe45d1f5fc1a45
Amira111 <amirajlassi190@yahoo.fr>
2016-11-30 22:29:09 +0100

    Update selection.php

--- selection.php
+++ selection.php
@@ -10,7 +10,7 @@ $sql = "SELECT * FROM os where name like 'Android'";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
 $row = mysqli_fetch_assoc($result);
-
+}
 echo json_encode($row);
 mysqli_close($conn);
 ?>
